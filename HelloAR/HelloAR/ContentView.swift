01128aa8f13be4dd91f17ba6ad57e6754b4b391f
hgunes <h.gunes13@gmail.com>
2022-08-14 22:55:47 +0200

    new: HelloAR

---
+++ HelloAR/HelloAR/ContentView.swift
@@ -0,0 +1,37 @@
+//
+//  ContentView.swift
+//  HelloAR
+//
+//  Created by Harun Gunes on 14/08/2022.
+//
+
+import SwiftUI
+import RealityKit
+
+struct ContentView : View {
+    var body: some View {
+        return ARViewContainer().edgesIgnoringSafeArea(.all)
+    }
+}
+
+struct ARViewContainer: UIViewRepresentable {
+    
+    func makeUIView(context: Context) -> ARView {
+        
+        let arView = ARView(frame: .zero)
+        
+        return arView
+        
+    }
+    
+    func updateUIView(_ uiView: ARView, context: Context) {}
+    
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
